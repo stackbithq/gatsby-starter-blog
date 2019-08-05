d21e4ef7bae8873ef07127cdb7ae55bae7024655
Robert Austin <rob@jugglerdigital.com>
2019-08-05 18:55:13 +1000

    extract site config to seperate json file so CMS can read/write data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
-  },
+  siteMetadata: require("./site-metadata.json"),
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
